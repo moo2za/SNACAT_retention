ab23f57faa8aeecc8a9728c6d193ad5df3bd057f
moo2za <62229594+moo2za@users.noreply.github.com>
2020-10-31 10:58:29 +0900

    2
    3

---
+++ apply.js
@@ -0,0 +1,31 @@
+const button = document.querySelector(`button`);
+
+
+function erchk(event) {
+    event.preventDefault();
+    if (document.getElementById("name").value == "") {
+        alert("이름을 입력해주세요");
+        return false;
+    }
+    else if(document.getElementById("email").value == "") {
+        alert("이메일을 입력해주세요");
+        return false;
+    }
+    else if(document.getElementById("phone").value == "") {
+        alert("전화번호를 입력해주세요");
+        return false;
+    }
+    else{
+    document.sub1.submit();
+    alert('제출이 완료되었습니다.');
+    }
+}
+
+
+
+function init() {
+    button.addEventListener("click", erchk);
+}
+  
+  
+init();
